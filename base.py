6e9d11b60fe3c089eb235fe0c20b7f971fdcc6f0
Akhil Ravidas <ar@dropbox.com>
2017-12-18 21:23:44 -0800

    recursion limit -> base

--- base.py
+++ base.py
@@ -57,3 +57,6 @@ clone = deepcopy
 # Grids
 dir_4 = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 dir_8 = [(dr, dc) for dr in range(-1, 2) for dc in range(-1, 2) if dr != 0 or dc != 0]
+
+import sys
+sys.setrecursionlimit(1000000000)
