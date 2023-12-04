62ac55c5da2bebdcc35ed0e6a22e9942afb94aa3
Akhil Ravidas <ar@mod0.ai>
2023-12-03 21:51:32 -0800

    Add a download script

---
+++ 2023/download.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+SESSION = os.environ["SESSION"]
+
+yr = 2023 if len(sys.argv) < 3 else sys.argv[2]
+day = sys.argv[1] if len(sys.argv) > 1 else int(input("Day: "))
+
+
+# Define the URL and headers for the request
+url = f"https://adventofcode.com/{yr}/day/{day}/input"
+headers = {
+    "Cookie": f"session={SESSION}",
+    "User-Agent": "src: https://github.com/akhilravidas/advent-of-code/blob/master/2023/download.py, reach me @ ar@mod0.ai",
+}
+
+response = requests.get(url, headers=headers, timeout=10)
+if response.status_code == 200:
+    output = response.text
+    with open(f"{day}.in", "w") as f:
+        f.write(output)
+    print("\n".join(output.split("\n")[:10]))
+else:
+    print(f"Error: Received status code {response.status_code}")
