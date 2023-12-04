4c1931251eb45044f1a96b203a5ed3be1c60ccfe
Akhil Ravidas <ar@mod0.ai>
2023-12-03 21:26:36 -0800

    Clean 4

--- 2023/4.1.py
+++ 2023/4.1.py
@@ -7,16 +7,10 @@ res = 0
 
 for row in inp:
     _, y = row.split(":")
-    y = y.strip()
     a, b = y.split("|")
-    a.strip()
-    b.strip()
-    good = set(int(x) for x in a.split(" ") if len(x))
-    cnt = 0
-    gg = [int(x) for x in b.split(" ") if len(x)]
-    for num in gg:
-        if num in good:
-            cnt += 1
+    good = set(int(x) for x in a.split())
+    gg = set(int(x) for x in b.split())
+    cnt = len(gg & good)
     if cnt > 0:
         res += 2 ** (cnt - 1)
 
--- 2023/4.2.py
+++ 2023/4.2.py
@@ -3,27 +3,16 @@ import sys
 
 inp = open(sys.argv[1]).read().strip().split("\n")
 
-res = 0
-
 copies = [1] * len(inp)
 
 for idx, row in enumerate(inp):
     _, y = row.split(":")
-    y = y.strip()
     a, b = y.split("|")
-    a.strip()
-    b.strip()
-    good = set(int(x) for x in a.split(" ") if len(x))
-    cnt = 0
-    gg = [int(x) for x in b.split(" ") if len(x)]
-    for num in gg:
-        if num in good:
-            cnt += 1
-    if cnt > 0:
-        res += 2 ** (cnt - 1)
-
-    for _i in range(cnt):
-        if idx + _i + 1 < len(copies):
-            copies[idx + _i + 1] += copies[idx]
+    good = set(int(x) for x in a.split())
+    gg = set(int(x) for x in b.split())
+    cnt = len(gg & good)
+    for j in range(cnt):
+        if idx + j + 1 < len(copies):
+            copies[idx + j + 1] += copies[idx]
 
 print(sum(copies))
